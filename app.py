8779bf467876c659fd5a9e955ce60b0a86399304
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 22:04:58 -0600

    f

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import streamlit as st
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
 st.sidebar.title('Software Educativo para el estudio de fenomenos ondulatorios')
 op1=st.sidebar.radio("",['Ondas Estacionarias','Interferencia de Ondas','Onda general'])
 if op1=='Ondas Estacionarias':
   st.title(op1)
+
   n=st.slider('',10^4,10^20)
-  x=np.linspace(0,2*np.pi,150)
+  x=np.linspace(0*np.pi,10^20)#DECLARAR VARIABLE, PUNTOS A GRAFICAR
  #st.write(x)
-  y=np.sin(2*pi/x)
+  y=np.sin(x)
   fig, ax = plt.subplots()
   ax.plot(x,y)
   st.pyplot(fig)
