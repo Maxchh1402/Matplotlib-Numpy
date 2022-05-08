e45394182ed84e63eccaa4d76adcc7f1d80b52b7
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 22:27:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import matplotlib.pyplot as plt
 st.sidebar.title('Software Educativo para el estudio de ondas electromagnéticas')
 op1=st.sidebar.radio("",['Ondas Estacionarias','Interferencia de Ondas','Onda general'])
 if op1=='Ondas Estacionarias':
-  st.title(op1)
-  col1, col2, col3 = st.columns([2,2,2])
-with col2:
-  st.image("https://qph.fs.quoracdn.net/main-qimg-47dfb28477b57ff310db4c3feb3ba157")
+st.image("https://qph.fs.quoracdn.net/main-qimg-47dfb28477b57ff310db4c3feb3ba157")
+st.write ("""Modifica la potencia 10^:  """)
 n=st.slider('',4,20)
-st.write ("""Modifica la potencia """)
 x=np.linspace(0,2*np.pi,150)
  #st.write(x)
 y=np.sin(n*x/2)
