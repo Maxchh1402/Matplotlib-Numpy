9ae24b4dd192bef1954647c94ac15abf47233b05
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 22:23:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-st.sidebar.title('Software Educativo para el estudio de fenomenos ondulatorios')
+st.sidebar.title('Software Educativo para el estudio de ondas electromagnéticas')
 op1=st.sidebar.radio("",['Ondas Estacionarias','Interferencia de Ondas','Onda general'])
 if op1=='Ondas Estacionarias':
   st.title(op1)
+  col1, col2, col3 = st.columns([3,6,6])
+with col2:
   st.image("https://qph.fs.quoracdn.net/main-qimg-47dfb28477b57ff310db4c3feb3ba157")
-  n=st.slider('',4,20)
-  st.write ("""Modifica la potencia """)
-  x=np.linspace(0,2*np.pi,150)
+n=st.slider('',4,20)
+st.write ("""Modifica la potencia """)
+x=np.linspace(0,2*np.pi,150)
  #st.write(x)
-  y=np.sin(n*x/2)
-  fig, ax = plt.subplots()
-  ax.plot(x,y)
-  st.pyplot(fig)
+y=np.sin(n*x/2)
+fig, ax = plt.subplots()
+ax.plot(x,y)
+st.pyplot(fig)
   
