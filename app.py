aa7d0d6af8facb00611c9ee8bb34757080ee2865
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 22:30:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 st.sidebar.title('Software Educativo para el estudio de ondas electromagnéticas')
 op1=st.sidebar.radio("",['Ondas electromagnéticas'])
 if op1=='Ondas electromagnéticas':
-st.image("https://qph.fs.quoracdn.net/main-qimg-47dfb28477b57ff310db4c3feb3ba157")
 st.write ("""Modifica la potencia 10^:  """)
+st.image("https://qph.fs.quoracdn.net/main-qimg-47dfb28477b57ff310db4c3feb3ba157")
 n=st.slider('',4,20)
 x=np.linspace(0,2*np.pi,150)
  #st.write(x)
